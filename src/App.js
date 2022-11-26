93ca8b46e760cc942c145de34bfd7012791029aa
Zak Fixler <zakfixler@gmail.com>
2022-11-26 16:03:05 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [searchInput, setSearchInput] = useState('');
+	const [results, setResults] = useState([]);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const url = 'https://naparc-puppeteer.onrender.com';
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ zip: searchInput }),
+		});
+		const data = await response.json();
+
+		setResults(data);
+	}
+
+	return (
+		<div>
+			<h1>Search for Congregations</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
+				/>
+				<button>Search</button>
+			</form>
+			{results.length &&
+				results[0].map((result, i) => {
+					return (
+						<div key={i}>
+							<h2>{result.name}</h2>
+              {result.pastor && <p>Pastor: {result.pastor}</p>}
+              {result.contact && <p>Contact: {result.contact}</p>}
+							<address>{result.address}</address>
+							<p>{result.phone}</p>
+              <a href={`mailto:${result.email}`}>Email</a>
+              <br />
+              <a href={result.website}>Website</a>
+						</div>
+					);
+				})}
+		</div>
+	);
 }
 
 export default App;
